aadf74d67d0c85a33ca9f0116a8d2c63ca373b16
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-25 22:47:06 +0300

    25_11_23

---
+++ src/components/board.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Board(props) {
+    return (
+        <main id="main-game" class="game-board">
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+        </main>
+    );
+}
+
+export default Board;
