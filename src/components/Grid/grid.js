b2db31e56c0e27de3a4add254f01751a7be99049
Strnikolay <nikolayromanchuk88@gmail.com>
2023-12-17 23:46:43 +0300

    17_12_23

--- src/components/Grid/grid.js
+++ src/components/Grid/grid.js
@@ -2,20 +2,10 @@
 export default class Grid {
   
   constructor(
-    gridElem,
-    {
-      gridSize = DEFAULT_GRID_SIZE,
-      cellSize = DEFAULT_CELL_SIZE,
-      cellGap = DEFAULT_CELL_GAP,
-    } = {}
+
   ) {
-    gridElem.innerHTML = ""
-    gridElem.style.setProperty("--grid-size", gridSize)
-    gridElem.style.setProperty("--cell-size", `${cellSize}vmin`)
-    gridElem.style.setProperty("--cell-gap", `${cellGap}vmin`)
-    this.#cells = createCellElements(gridElem, gridSize).map((cell, index) => {
-      return new Cell(cell, index % gridSize, Math.floor(index / gridSize))
-    })
+
+    }
   }
   
     get colCells() {
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import "./board.css"
 
+const Cell = (tile, x, y) =>{
+    return (<div class="cell">{tile}</div>)
+} 
+
 function Board(props) {
     const gameBoard = document.getElementById("main-game")
+    const board = [
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null},
+        {x:1, y:1, tile: null}
+    ] 
+    
     return (
         <main id="main-game" class="game-board">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
         </main>
     );
 }
