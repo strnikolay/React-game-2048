7a3c17a3ede1ad1c445b69cbd5310d58191ca67f
Проходная <nikolayromanchuk88@gmail.com>
2023-12-18 23:55:17 +0300

    cell

--- src/components/Grid/cell.js
+++ src/components/Grid/cell.js
@@ -1,5 +1,48 @@
 
 export default class Cell {
-
-
-}
+    #tile
+    #mergeTile
+    constructor(cell, x, y) {
+      this.cell = cell
+      this.x = x
+      this.y = y
+    }
+  
+    get tile() {
+      return this.#tile
+    }
+  
+    set tile(value) {
+      this.#tile = value
+      if (value == null) return
+      this.#tile.x = this.x
+      this.#tile.y = this.y
+    }
+  
+    get mergeTile() {
+      return this.#mergeTile
+    }
+  
+    set mergeTile(value) {
+      this.#mergeTile = value
+      if (value == null) return
+      this.#mergeTile.x = this.x
+      this.#mergeTile.y = this.y
+    }
+  
+    canAccept(tile) {
+      return (
+        this.tile == null ||
+        (this.mergeTile == null && this.tile.value === tile.value)
+      )
+    }
+  
+    mergeTiles() {
+      if (this.tile == null || this.mergeTile == null) return 0
+      this.tile.value = this.tile.value + this.mergeTile.value
+      this.mergeTile.remove()
+      this.mergeTile = null
+      this.tile.pop()
+      return this.tile.value
+    }
+  }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -5,26 +5,31 @@ const Cell = (tile, x, y) =>{
     return (<div class="cell">{tile}</div>)
 } 
 
+const board = [
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null},
+    {x:1, y:1, tile: null}
+] 
+
+const DEFAULT_GRID_SIZE = 4
+const DEFAULT_CELL_SIZE = 20
+const DEFAULT_CELL_GAP = 2
+
 function Board(props) {
     const gameBoard = document.getElementById("main-game")
-    const board = [
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null},
-        {x:1, y:1, tile: null}
-    ] 
     
     return (
         <main id="main-game" class="game-board">
